66c4dd5d5da58cbc959c9dee8a9e9e7ca1805650
Jakub Sedlařík <jakub.sedlarik@sanasport.cz>
2017-02-23 12:41:07 +0100

    Utils Money with method toCents

---
+++ src/Utils/Money.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Markette\GopayInline\Utils;
+
+final class Money
+{
+	/**
+	 * @param float $amout
+	 * @return float
+	 */
+	public static function toCents($amout)
+	{
+		return round($amout * 100);
+	}
+}
