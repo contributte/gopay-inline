e542cd18bda3a4986852c265cf615141df056053
Michal Haltuf <haltuf@imagineo.cz>
2017-03-07 21:31:48 +0100

    whitespaces

--- src/Api/Lists/Format.php
+++ src/Api/Lists/Format.php
@@ -10,22 +10,22 @@ class Format
 
 	// CSV typ B
 	const CSV_B = 'CSV_B';
-	
+
 	// CSV typ C
 	const CSV_C = 'CSV_C';
-	
+
 	// CSV typ D
 	const CSV_D = 'CSV_D';
-	
+
 	// XLS typ A
 	const XLS_A = 'XLS_A';
-	
+
 	// XLS typ B
 	const XLS_B = 'XLS_B';
-	
+
 	// XLS typ C
 	const XLS_C = 'XLS_C';
-	
+
 	// ABO (.gpc)
 	const ABO_A = 'ABO_A';
 
